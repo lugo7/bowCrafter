5589af6a1982dfe27015e587586fc03a51e1f9f9
Michael Lugo <lugo.michael.77@gmail.com>
2022-04-07 19:02:19 -0400

    Fixed collapsible content

--- script.js
+++ script.js
@@ -9,20 +9,31 @@ function loadIn(item){
   slideIn(item);
 }
 
+function active(){
+  $('.imageRow').style.height='200px';
+  $('#spacer').style.height='10px';
+}
+
 document.addEventListener("DOMContentLoaded", function(){
   for(let i=0;i<itemSection.length;i++){
-    setInterval(loadIn(itemSection[i]),1000);6
+    setInterval(loadIn(itemSection[i]),1000);
   }
   for(let i=0;i<itemSection.length;i++){
     itemSection[i].addEventListener('click',function(elem){
       if(elem.target.id=='one'){
         $('#oneContent').toggle('slow');
+        $('#oneB').classList.toggle('active');
+        active();
       }
-      else if (elem.target.id=='two') {
+      else if(elem.target.id=='two') {
         $('#twoContent').toggle('slow');
+        $('#twoB').classList.toggle('active');
+        active();
       }
       else if(elem.target.id=='three'){
         $('#threeContent').toggle('slow');
+        $('#threeB').classList.toggle('active');
+        active();
       }
     });
   }
